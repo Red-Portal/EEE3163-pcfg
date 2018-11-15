7162ae6ebb609d2e9f2d0991d19beeef41e4485e
Jun3E <brdkllr133@gmail.com>
2018-11-15 13:46:46 +0900

    molla

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,10 +1,14 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 -intstyle "ise" -incremental -o "C:/Users/JUNE/Documents/GitHub/EEE3163-pcfg/memory_block_test_isim_beh.exe" -prj "C:/Users/JUNE/Documents/GitHub/EEE3163-pcfg/memory_block_test_beh.prj" "work.memory_block_test" 
 =======
 -intstyle "ise" -incremental -o "/home/msca8h/Projects/EEE3163-pcfg/tb181111_01forTOP_8254_isim_beh.exe" -prj "/home/msca8h/Projects/EEE3163-pcfg/tb181111_01forTOP_8254_beh.prj" "work.tb181111_01forTOP_8254" 
 >>>>>>> dfa3576e34fde7fbf602852b7ac6f12b5acb792c
+=======
+-intstyle "ise" -incremental -o "/home/msca8h/Projects/EEE3163-pcfg/address_decoder_test_isim_beh.exe" -prj "/home/msca8h/Projects/EEE3163-pcfg/address_decoder_test_beh.prj" "work.address_decoder_test" 
+>>>>>>> parent of dfa3576... finished top module - no simulation performed yet.
 =======
 -intstyle "ise" -incremental -o "/home/msca8h/Projects/EEE3163-pcfg/address_decoder_test_isim_beh.exe" -prj "/home/msca8h/Projects/EEE3163-pcfg/address_decoder_test_beh.prj" "work.address_decoder_test" 
 >>>>>>> parent of dfa3576... finished top module - no simulation performed yet.
