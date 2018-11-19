61b2b457e41be17758ab95343deefe225faaa6f7
Jun3E <brdkllr133@gmail.com>
2018-11-20 01:43:25 +0900

    controller pc : state output removed

--- controller_pc.vhd
+++ controller_pc.vhd
@@ -43,12 +43,13 @@ entity controller_pc is
         count_ram_ce  : out STD_LOGIC;
         count_data_ce : out STD_LOGIC);
 end controller_pc;
+
 architecture Behavioral of controller_pc is
   type state_t is (st_idle,
                    st_ready,
                    st_r,
                    st_w,
-                   st_wait,
+                   st_waitw,
                    st_cntr,
                    st_cntw);
 
@@ -56,8 +57,36 @@ architecture Behavioral of controller_pc is
   signal next_state    : state_t;
 
   signal s_debug_clk : std_logic;
+  
+  
+--  signal s_count_ram_ce : std_logic;
+--  signal s_count_data_ce : std_logic;
+--  signal s_ram_ena : std_logic;
+--  signal s_ram_wea : std_logic_vector(0 downto 0);
+--  signal s_ram_enb : std_logic;
+--  signal s_mux_ram_sel : std_logic;
 
 begin 
+  count_ram_ce <= '1' when (current_state = st_cntr) else
+						'1' when (current_state = st_cntw) else
+						'0';
+						
+  count_data_ce <= '1' when (current_state = st_cntw) else
+						 '0';
+						 
+  ram_ena <= '1' when (current_state = st_w) else
+				 '0';
+  
+  ram_wea <= "1" when (current_state = st_w) else
+				 "0";
+  
+  ram_enb <= '1' when (current_state = st_r) else
+				 '0';
+  
+  mux_ram_sel <= '1' when (current_state = st_r) else
+					  '1' when (current_state = st_w) else
+					  '0';
+  
   clk_proc: process(s_clk, m_reset)
   begin
     if(m_reset = '1') then
@@ -80,12 +109,12 @@ begin
     --if(rising_edge(s_clk)) then
     case current_state is
       when st_idle =>
-        count_ram_ce  <= '0';
-        count_data_ce <= '0';
-        ram_ena       <= '0';
-        ram_wea       <= "0";
-        ram_enb       <= '0';
-        mux_ram_sel   <= '0';
+--        count_ram_ce  <= '0';
+--        count_data_ce <= '0';
+--        ram_ena       <= '0';
+--        ram_wea       <= "0";
+--        ram_enb       <= '0';
+--        mux_ram_sel   <= '0';
 
         if(ctrl_startio = '1') then
           next_state <= st_ready;
@@ -94,9 +123,9 @@ begin
         end if;
 
       when st_ready =>
-        ram_enb       <= '1';
-        count_ram_ce  <= '0';
-        count_data_ce <= '0';
+--        ram_enb       <= '1';
+--        count_ram_ce  <= '0';
+--        count_data_ce <= '0';
 
         if((s_ren = '1') AND (ctrl_startio = '1')) then
           next_state <= st_r;
@@ -107,8 +136,8 @@ begin
         end if;
         
       when st_r =>
-        ram_enb       <= '0';
-        mux_ram_sel   <= '1';
+--        ram_enb       <= '0';
+---        mux_ram_sel   <= '1';
         
         if(s_ren = '0') then
           next_state <= st_cntr;
@@ -117,33 +146,34 @@ begin
         end if;
 
       when st_w =>
-        ram_enb       <= '0';
-        ram_ena       <= '1';
-        ram_wea       <= "1";
+--        ram_enb       <= '0';
+---        ram_ena       <= '1';
+---        ram_wea       <= "1";
 
-        mux_ram_sel   <= '1';
-        next_state <= st_wait;
+---        mux_ram_sel   <= '1';
+		  
+        next_state <= st_waitw;
 
-      when st_wait =>
-        ram_ena       <= '0';
-        ram_wea       <= "0";
-        mux_ram_sel   <= '0';
+      when st_waitw =>
+--        ram_ena       <= '0';
+--        ram_wea       <= "0";
+--        mux_ram_sel   <= '0';
 
-        if(s_ren = '1') then
-          next_state <= st_wait;
+        if(s_wen = '1') then
+          next_state <= st_waitw;
         else
           next_state <= st_cntw;
         end if;
 
       when st_cntr =>
-        count_ram_ce  <= '1';
-        mux_ram_sel   <= '0';
+--        count_ram_ce  <= '1';
+--        mux_ram_sel   <= '0';
         next_state   <= st_idle;
 
       when st_cntw =>
-        count_ram_ce  <= '1';
-        count_data_ce <= '1';
-        mux_ram_sel   <= '0';
+--        count_ram_ce  <= '1';
+--        count_data_ce <= '1';
+--        mux_ram_sel   <= '0';
         next_state   <= st_idle;
 
       when others =>
