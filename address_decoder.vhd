6f714595920b50e1c5e80023fbe7f701b5b7acd2
Ray Kim <msca8h@naver.com>
2018-11-24 17:52:04 +0900

    removed redundant comments

--- address_decoder.vhd
+++ address_decoder.vhd
@@ -1,35 +1,7 @@
 
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:14:55 11/14/2018 
--- Design Name: 
--- Module Name:    address_decoder - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity address_decoder is
     Port (s_address           : in  STD_LOGIC_VECTOR (8 downto 0);
